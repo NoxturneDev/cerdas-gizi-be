864b1db33c0c5dde2e6c413139c2e39c297a1edd
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-12-18 11:33:46 +0700

    feat: created first version of prompt system

--- controller/prompt-controller.js
+++ controller/prompt-controller.js
@@ -1,34 +1,39 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const {GoogleGenerativeAI} = require("@google/generative-ai");
+const fs = require("fs");
 
 async function scanImage(req, res) {
-  // const imageBuffer = Buffer.from(req.body.file.content).toString('base64');
+  const {body} = req;
 
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
 
-  const model = genAI.getGenerativeModel({model: 'gemini-pro'});
-  const prompt = req.body.question;
+  // const model = genAI.getGenerativeModel({model: 'gemini-pro'});
+  // const prompt = req.body.question;
 
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
+  // const result = await model.generateContent(prompt);
+  // const response = await result.response;
+  // const text = response.text();
 
-  //
-  // const imageData =
-  //     inlineData: {
-  //       data: imageBuffer,
-  //       mimeType
-  //     },
-  //   };
-  // }
+  const imageData = {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync('./test-3.jpeg')).toString('base64'),
+      mimeType: 'image/jpeg',
+    },
+  };
 
+  const imageModel = genAI.getGenerativeModel({model: 'gemini-pro-vision'});
+  const imageModelGenerate = await imageModel.generateContent([body?.prompt, imageData]);
 
+  const result = await imageModelGenerate.response;
+  const text = result.text();
+
+  console.log("result: ", text);
   try {
     return res.status(200).json({
       message: 'message',
       status: 'OK',
       genAIresponse: text,
     })
-  } catch(error) {
+  } catch (error) {
     console.log(error);
   }
 }
