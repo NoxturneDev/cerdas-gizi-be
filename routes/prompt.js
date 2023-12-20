8ec9437661e0f9acf8b105b492ecf922f70ecbfd
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-12-20 07:20:09 +0700

    feat: added handleImage middleware to prompt route

--- routes/prompt.js
+++ routes/prompt.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const imageHandler = require('../services/imageHandler');
 const PromptController = require('../controller/prompt-controller');
 
-router.post('/api/prompt/image', PromptController.scanImage);
+router.post('/api/prompt/image', imageHandler.upload.single('file'), PromptController.scanImage);
 
 module.exports = router;
