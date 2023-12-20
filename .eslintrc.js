7dd09e71d5376628652d6869bbf07246758c3023
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-12-20 08:44:31 +0700

    feat: init eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': 0,
+    semi: 'error',
+    'no-console': 0,
+    quotes: 1,
+    'max-len': 0,
+  },
+};
