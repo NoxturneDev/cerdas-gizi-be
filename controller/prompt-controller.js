c22c24b9b3910c74aad0f56573f7f02c57a03417
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-12-20 07:19:49 +0700

    feat: created handle image system boilerplate
    + added sharp library
    + added multer configuration

--- controller/prompt-controller.js
+++ controller/prompt-controller.js
@@ -1,9 +1,9 @@
 const {GoogleGenerativeAI} = require("@google/generative-ai");
-const fs = require("fs");
+const sharp = require('sharp');
 
 async function scanImage(req, res) {
   const {body} = req;
-  // const imageBuffer = Buffer.from(req.body.file.content).toString('base64');
+  const imageBuffer = Buffer.from(req.body.file.buffer).toString('base64');
 
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
 
@@ -14,25 +14,28 @@ async function scanImage(req, res) {
   // const response = await result.response;
   // const text = response.text();
 
-  const imageData = {
-    inlineData: {
-      data: Buffer.from(fs.readFileSync('./test-3.jpeg')).toString('base64'),
-      mimeType: 'image/jpeg',
-    },
-  };
+  // const imageData = {
+  //   inlineData: {
+  //     data: Buffer.from(fs.readFileSync('./test-3.jpeg')).toString('base64'),
+  //     mimeType: 'image/jpeg',
+  //   },
+  // };
 
-  const imageModel = genAI.getGenerativeModel({model: 'gemini-pro-vision'});
-  const imageModelGenerate = await imageModel.generateContent([body?.prompt, imageData]);
+  // const imageModel = genAI.getGenerativeModel({model: 'gemini-pro-vision'});
+  // const imageModelGenerate = await imageModel.generateContent([body?.prompt, imageData]);
+  //
+  // const result = await imageModelGenerate.response;
+  // const text = result.text();
 
-  const result = await imageModelGenerate.response;
-  const text = result.text();
+  // console.log("result: ", text);
 
-  console.log("result: ", text);
   try {
+    await sharp(imageBuffer).resize({width:  250, height: 250}).png().toFile(`./public/data/uploads/test-upload`);
+
     return res.status(200).json({
       message: 'message',
       status: 'OK',
-      genAIresponse: text,
+      // genAIresponse: text,
     })
   } catch (error) {
     console.log(error);
---
+++ services/imageHandler.js
@@ -0,0 +1,9 @@
+const multer = require('multer');
+
+const upload = multer({dest: './public/data/uploads/'});
+
+module.exports = {upload};
+
+
+
+
