70a40bbc1b0785e3050d4a835fa8571fa98721d0
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-12-22 10:59:32 +0700

    feat: added authentication middleware

--- core/routes.js
+++ core/routes.js
@@ -1,15 +1,23 @@
 const express = require('express');
+
 const router = express.Router();
 const promptRouter = require('../app/prompt/prompt-routes');
 const userRouter = require('../app/user/user-routes');
+const { authentication } = require('../app/services/auth-service');
+const userController = require('../app/user/user-controller');
+
+// auth routes
+router.post('/logout', userController.logoutUser);
+router.post('/login', userController.loginUser);
+router.post('/register', userController.registerUser);
+
+router.use('/v1', async (req, res, next) => { await authentication(req, res, next); });
 
 router.use(promptRouter);
 router.use(userRouter);
 
-router.get('/api/check', async (req, res) => {
-  return res.status(200).json({
-    message: "Server is good to go now, 🚀 berkat ashue fahmi ashue",
-  })
-})
+router.get('/api/check', async (req, res) => res.status(200).json({
+  message: 'Server is good to go now, 🚀 berkat ashue fahmi ashue',
+}));
 
 module.exports = router;
