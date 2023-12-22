a26826ff2d08ca0f897e8c76d9416b35246ec087
Berkat Saragih <berkat.sragih@gmail.com>
2023-12-22 14:16:42 +0700

    refactor: user domain

--- app/user/user-controller.js
+++ app/user/user-controller.js
@@ -1,4 +1,3 @@
-const jwt = require('jsonwebtoken');
 const userService = require('./user-service');
 
 // Test Passed
@@ -50,23 +49,6 @@ async function getUserById(req, res) {
 }
 
 // Test Passed
-async function registerUser(req, res) {
-  const newUser = await userService.registerNewUser(req.body);
-
-  if (newUser.error) {
-    return res.status(newUser.error.statusCode).json({
-      status: 'failed',
-      message: newUser.error.message,
-      users: [],
-    });
-  }
-
-  return res.status(201).json({
-    status: 'success',
-    message: 'new user data added!',
-    users: newUser,
-  });
-}
 
 // Test Passed
 async function updateDataUserById(req, res) {
@@ -110,62 +92,62 @@ async function deleteDataUserById(req, res) {
 }
 
 // USER SERVICES API CONTROLLER
-async function loginUser(req, res) {
-  const { phoneNumber, password } = req.body;
+// async function loginUser(req, res) {
+//   const { phoneNumber, password } = req.body;
+//
+//   const loginResp = await userService.loginUser({ phoneNumber, password });
+//
+//   if (loginResp.error) {
+//     return res.status(loginResp.error.statusCode).json({
+//       status: 'failed',
+//       message: loginResp.error.message,
+//       users: {},
+//     });
+//   }
+//
+//   // res.cookie('refreshToken', loginResp.refreshToken, {
+//   //   httpOnly: true,
+//   //   maxAge: 24 * 60 * 60 * 1000,
+//   //   secure: true,
+//   //   sameSite: 'None',
+//   // });
+//
+//   return res.status(200).json({
+//     status: 'success',
+//     message: 'login success',
+//     user: loginResp.user,
+//     accessToken: loginResp.accessToken,
+//   });
+// }
 
-  const loginResp = await userService.loginUser({ phoneNumber, password });
-
-  if (loginResp.error) {
-    return res.status(loginResp.error.statusCode).json({
-      status: 'failed',
-      message: loginResp.error.message,
-      users: {},
-    });
-  }
-
-  // res.cookie('refreshToken', loginResp.refreshToken, {
-  //   httpOnly: true,
-  //   maxAge: 24 * 60 * 60 * 1000,
-  //   secure: true,
-  //   sameSite: 'None',
-  // });
-
-  return res.status(200).json({
-    status: 'success',
-    message: 'login success',
-    user: loginResp.user,
-    accessToken: loginResp.accessToken,
-  });
-}
-
-async function logoutUser(req, res) {
-  try {
-    const { userId } = req.body;
-
-    const logoutResult = await userService.logoutUser(userId);
-
-    if (logoutResult.error) {
-      return res.status(logoutResult.error.statusCode).json({
-        status: 'failed logout',
-        message: logoutResult.error.message,
-        users: {},
-      });
-    }
-
-    res.clearCookie('refreshToken');
-    return res.status(200).json({
-      status: 'success',
-      message: 'Success Logout!',
-
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      status: 'failed',
-      message: error.message,
-    });
-  }
-}
+// async function logoutUser(req, res) {
+//   try {
+//     const { userId } = req.body;
+//
+//     const logoutResult = await userService.logoutUser(userId);
+//
+//     if (logoutResult.error) {
+//       return res.status(logoutResult.error.statusCode).json({
+//         status: 'failed logout',
+//         message: logoutResult.error.message,
+//         users: {},
+//       });
+//     }
+//
+//     res.clearCookie('refreshToken');
+//     return res.status(200).json({
+//       status: 'success',
+//       message: 'Success Logout!',
+//
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({
+//       status: 'failed',
+//       message: error.message,
+//     });
+//   }
+// }
 
 // DEVELOPMENT ONLY API
 // async function deleteAllUserData(req, res) {
@@ -188,10 +170,7 @@ async function logoutUser(req, res) {
 module.exports = {
   getAllUsers,
   getUserById,
-  registerUser,
-  logoutUser,
   updateDataUserById,
   deleteDataUserById,
   // deleteAllUserData,
-  loginUser,
 };
--- app/user/user-service.js
+++ app/user/user-service.js
@@ -2,9 +2,7 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable consistent-return */
 /* eslint-disable no-use-before-define */
-const { v4: uuidv4 } = require('uuid');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const { ApiError } = require('../lib/Error');
 const queryHandler = require('../lib/queryHandler');
 const responseDataMapper = require('../lib/responseDataMapper');
@@ -99,115 +97,100 @@ async function deleteUser(userId) {
   }
 }
 
-async function logoutUser(userId) {
-  try {
-    const userLogout = await UserRepository.getUserById(userId, 'userId');
-
-    if (!userLogout) {
-      throw new ApiError(404, `user with id ${userId} is not found`, true);
-    }
-
-    return responseDataMapper(userLogout, ['userId', 'name']);
-  } catch (error) {
-    console.log(error);
-    return { error: { message: error.message, statusCode: error.statusCode || 400 } };
-  }
-}
-
 // TODO: Logout functionality
 // LOGIN & REGISTER
-async function loginUser(credentials) {
-  try {
-    const { phoneNumber, password } = credentials;
+// async function loginUser(credentials) {
+//   try {
+//     const { phoneNumber, password } = credentials;
+//
+//     const user = await UserRepository.getUserByPhoneNumber(phoneNumber);
+//
+//     if (!user) {
+//       throw new ApiError(404, `user with username ${phoneNumber} is not found`, true);
+//     }
+//
+//     const passwordCheck = await passwordChecker(password, user.password);
+//     if (!passwordCheck) {
+//       throw new ApiError(403, 'wrong password');
+//     }
+//
+//     const accessToken = jwt.sign({
+//       userId: user.userId,
+//     }, process.env.SECRET_ACCESS_TOKEN, {
+//       expiresIn: '30d',
+//     });
+//
+//     // todo : send roles permission data also
+//     const refreshToken = jwt.sign({ id: user.id }, process.env.SECRET_REFRESH_TOKEN, {
+//       expiresIn: '40d',
+//     });
+//
+//     await UserRepository.updateUserById(user.userId, { refreshToken });
+//
+//     return {
+//       user, accessToken,
+//     };
+//   } catch (error) {
+//     console.log(error);
+//     return { error: { message: error.message, statusCode: error.statusCode || 400 } };
+//   }
+// }
 
-    const user = await UserRepository.getUserByPhoneNumber(phoneNumber);
-
-    if (!user) {
-      throw new ApiError(404, `user with username ${phoneNumber} is not found`, true);
-    }
-
-    const passwordCheck = await passwordChecker(password, user.password);
-    if (!passwordCheck) {
-      throw new ApiError(403, 'wrong password');
-    }
-
-    const accessToken = jwt.sign({
-      userId: user.userId,
-    }, process.env.SECRET_ACCESS_TOKEN, {
-      expiresIn: '30d',
-    });
-
-    // todo : send roles permission data also
-    const refreshToken = jwt.sign({ id: user.id }, process.env.SECRET_REFRESH_TOKEN, {
-      expiresIn: '40d',
-    });
-
-    await UserRepository.updateUserById(user.userId, { refreshToken });
-
-    return {
-      user, accessToken,
-    };
-  } catch (error) {
-    console.log(error);
-    return { error: { message: error.message, statusCode: error.statusCode || 400 } };
-  }
-}
-
-async function registerNewUser(data) {
-  const filteredReq = filterRequestBody(data, ['password']);
-  const userId = data.userId !== undefined ? data.userId : uuidv4();
-  const createdAt = new Date();
-  const updatedAt = createdAt;
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const cryptedPassword = await bcrypt.hash(data.password, salt);
-
-    /**
-     * @type {UsersCreateInput}
-     */
-    const newUserData = {
-      ...filteredReq, userId, createdAt, updatedAt, password: cryptedPassword,
-    };
-
-    // const existingPhoneNumber = await checkExistingPhoneNumber(data.phoneNumber);
-
-    // if (existingPhoneNumber) {
-    //   throw new ApiError(400, 'phone number already exist', true);
-    // }
-
-    const newUser = await UserRepository.createNewUser(newUserData);
-
-    if (newUser.error) {
-      throw new ApiError('400', newUser.error?.message, true);
-    }
-
-    return responseDataMapper(newUser, ['userId', 'phoneNumber', 'name']);
-  } catch (error) {
-    console.log(error);
-    return { error: { message: error.message, statusCode: error.statusCode || 400 } };
-  }
-}
+// async function registerNewUser(data) {
+//   const filteredReq = filterRequestBody(data, ['password']);
+//   const userId = data.userId !== undefined ? data.userId : uuidv4();
+//   const createdAt = new Date();
+//   const updatedAt = createdAt;
+//
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     const cryptedPassword = await bcrypt.hash(data.password, salt);
+//
+//     /**
+//      * @type {UsersCreateInput}
+//      */
+//     const newUserData = {
+//       ...filteredReq, userId, createdAt, updatedAt, password: cryptedPassword,
+//     };
+//
+//     // const existingPhoneNumber = await checkExistingPhoneNumber(data.phoneNumber);
+//
+//     // if (existingPhoneNumber) {
+//     //   throw new ApiError(400, 'phone number already exist', true);
+//     // }
+//
+//     const newUser = await UserRepository.createNewUser(newUserData);
+//
+//     if (newUser.error) {
+//       throw new ApiError('400', newUser.error?.message, true);
+//     }
+//
+//     return responseDataMapper(newUser, ['userId', 'phoneNumber', 'name']);
+//   } catch (error) {
+//     console.log(error);
+//     return { error: { message: error.message, statusCode: error.statusCode || 400 } };
+//   }
+// }
 
 // ----------------------------------------------------------------------
 // utility functions for user service
 // ----------------------------------------------------------------------
-async function checkExistingPhoneNumber(phoneNumber) {
-  const getExistingUser = await UserRepository.getUserByPhoneNumber(phoneNumber);
+// async function checkExistingPhoneNumber(phoneNumber) {
+//   const getExistingUser = await UserRepository.getUserByPhoneNumber(phoneNumber);
+//
+//   if (getExistingUser) return true;
+//
+//   return false;
+// }
 
-  if (getExistingUser) return true;
-
-  return false;
-}
-
-async function passwordChecker(passwordToCheck, correctPassword) {
-  const match = await bcrypt.compare(passwordToCheck, correctPassword);
-
-  if (!match) return false;
-
-  return true;
-}
+// async function passwordChecker(passwordToCheck, correctPassword) {
+//   const match = await bcrypt.compare(passwordToCheck, correctPassword);
+//
+//   if (!match) return false;
+//
+//   return true;
+// }
 
 module.exports = {
-  getAllUsers, getUserById, logoutUser, updateUserInformation, deleteUser, registerNewUser, loginUser,
+  getAllUsers, getUserById, updateUserInformation, deleteUser,
 };
