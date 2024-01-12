07d412de50bf03ed3ae40e832668db62df810e0d
Galih Adhi Kusuma <noxturne.production@gmail.com>
2024-01-12 21:11:42 +0700

    feat: created basic prompt controller

--- app/prompt/prompt-controller.js
+++ app/prompt/prompt-controller.js
@@ -1,16 +1,15 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 const fs = require('fs');
 
+function replaceParameters(promptDesign, replacements) {
+  return Object.entries(replacements).reduce((result, [parameter, data]) => {
+    const regex = new RegExp(`#${parameter}#`, 'g');
+    return result.replace(regex, data);
+  }, promptDesign);
+}
+
 async function scanImage(req, res) {
   const { body } = req;
-  // const imageBuffer = Buffer.from([req.file]).toString('base64');
-
-  // if (!req.file || req.file?.filename) {
-  //   return res.status(400).json({
-  //     message: 'file not found',
-  //     status: 'FAILED',
-  //   });
-  // }
   try {
     const buffer = Buffer.from(fs.readFileSync(`./public/data/uploads/${req.file?.filename}`)).toString('base64');
     const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
@@ -22,18 +21,31 @@ async function scanImage(req, res) {
       },
     };
 
+    const rawPrompt = body.prompt;
+    const promptDesignBase = 'send the response in JSON (no need to add json as a prefix for the answer) with object of : advice, nutritions fact (object), children data (name, age). response in BAHASA INDONESIA. this is the prompt: Hi, i WANT you to act like my private nutritionist and give me some healthy advice according to food and nutrients. First of all, my kid is at #age# and his name is #name#. I wanted to give him this food at the picture. and this is the further description about it. #prompt#. please also write some advice is this food is recommended based on the nutrition. Im concerned about stunting issue, so I want my children to be as healthy as possible.';
+
+    const replacements = {
+      prompt: rawPrompt,
+      name: 'Galih Adhi kusuma',
+      age: 9,
+    };
+
+    const finalPrompt = replaceParameters(promptDesignBase, replacements);
+
     const imageModel = genAI.getGenerativeModel({ model: 'gemini-pro-vision' });
-    const imageModelGenerate = await imageModel.generateContent([body?.prompt, imageData]);
-    const { totalTokens } = await imageModel.countTokens([body?.prompt, imageData]);
-    //
+    const imageModelGenerate = await imageModel.generateContent([finalPrompt, imageData]);
+    const { totalTokens } = await imageModel.countTokens([finalPrompt, imageData]);
+
     const result = await imageModelGenerate.response;
     const text = result.text();
 
     return res.status(200).json({
       message: 'message',
       status: 'OK',
-      genAIresponse: text,
+      data: text,
       tokens: totalTokens,
+      prompt: finalPrompt,
+      // jsonFormat: JSON.parse(text),
     });
   } catch (error) {
     console.log(error);
