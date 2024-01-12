6d0bf42eb9ce4324aa8cf37758bc33ec7eadff0f
Galih Adhi Kusuma <noxturne.production@gmail.com>
2024-01-12 13:19:43 +0700

    feat: created basic prompt-image handler

--- app/prompt/prompt-routes.js
+++ app/prompt/prompt-routes.js
@@ -3,5 +3,6 @@ const imageHandler = require('../lib/imageHandler');
 const PromptController = require('./prompt-controller');
 
 router.post('/api/prompt/image', imageHandler.upload.single('file'), PromptController.scanImage);
+// router.post('/api/prompt/image', PromptController.scanImage);
 
 module.exports = router;
